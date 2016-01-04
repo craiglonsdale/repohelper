783bef5ec2767971e20c2e7016d093d60cab80ac
Craig Lonsdale <craig@meerkats.com.au>
2016-01-04 14:39:02 +0800

    Moved api and tests across

---
+++ tests/tests.js
@@ -0,0 +1,31 @@
+'use strict'; //eslint-disable-line
+const expect = require('chai').expect;
+
+describe('RepoHelper', () => {
+  describe('print module', () => {
+    let stdoutWrite;
+    let log;
+    before(() => {
+      stdoutWrite = process.stdout.write;
+      process.stdout.write = (message) => {
+        log.push(message);
+      };
+    });
+    beforeEach(() => log = []);
+    afterEach(() => log = []);
+    after(() => process.stdout.write = stdoutWrite);
+    const print = require('../lib/print');
+    it('Should write to stdout', () => {
+      print('Hello world', true);
+      expect(log.slice(2).join('')).to.eql('Hello world\n');
+    });
+    it('should obey the conditional flag', () => {
+      print('Hello world', false);
+      expect(log.length).to.eql(0);
+    });
+    it('should concatenate an array', () => {
+      print(['Hello', ' ', 'world'], true);
+      expect(log.slice(-1).toString()).to.eql('Hello world\n');
+    });
+  });
+});
